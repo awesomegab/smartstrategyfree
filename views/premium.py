dbd1b93627fd50605182dbca86e9b6689ef00e7e
awesomegab <gm100327@gmail.com>
2024-09-06 08:35:03 -0300

    Update premium.py

--- views/premium.py
+++ views/premium.py
@@ -8,9 +8,10 @@ col1, col2 = st.columns(2, gap="small", vertical_alignment="top")
 
 with col1:
     st.link_button("Premium", "https://smartstrategy.streamlit.app")
-    st.image("assets/premium advert.png")
+    st.video("https://youtu.be/0yPaWaoc0RY")
     st.link_button("Premium", "https://smartstrategy.streamlit.app")
 
 with col2:
     st.title("")
-    st.video("https://youtu.be/0yPaWaoc0RY")
+    st.image("assets/premium advert.png")
+    st.link_button("Premium", "https://smartstrategy.streamlit.app")
