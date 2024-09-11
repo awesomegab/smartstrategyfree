0005d35962388d68eac64fc56094d0dd30dbe761
awesomegab <gm100327@gmail.com>
2024-09-11 08:58:28 -0300

    Update premium.py

--- views/premium.py
+++ views/premium.py
@@ -8,6 +8,8 @@ col1, col2 = st.columns(2, gap="small", vertical_alignment="top")
 
 with col1:
     st.title("")
+    st.write("""Se você veio do Instagram mobile é necessário abrir o link no navegador padrão do dipositivo,
+    toque nos três pontos no canto superior direito e selecione 'Abrir no navegador'.""")
     st.link_button("Premium", "https://smartstrategy.streamlit.app")
     st.video("https://youtu.be/0yPaWaoc0RY")
 
